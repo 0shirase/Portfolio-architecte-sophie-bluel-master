b1dff37ab011dc405a65668638d9ef1ac2be7f83
0shirase <lucas0999.lb@gmail.com>
2023-12-04 14:48:11 +0100

    ajout des catégories dynamiques

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,14 +1,15 @@
-const gallery = document.querySelector('gallery');
+const gallery = document.querySelector('.gallery');
 
 async function main () {
-    displayworks ();
+    displayWorks ();
+    buttonfilters ();
 }
 
 main();
 
-/*** appel de l'API ***/
+/***appel de l'API***/
 
-/**works ***/
+/**works **/
 async function getWorks () {
     try {
         const worksResponse = await fetch ("http://localhost:5678/api/works");
@@ -19,8 +20,9 @@ async function getWorks () {
     }
 }
 
-/**category */
+/**category**/
 async function getCategories () {
+    
     try {
         const categoriesResponse = await fetch ("http://localhost:5678/api/categories");
         return categoriesResponse.json();
@@ -29,7 +31,7 @@ async function getCategories () {
         console.log("Erreur durant la récupération des catégories dans l'API")
     }
 }
-/***Affichage dynamique de la gallery ***/
+/***Affichage dynamique de la gallery***/
 async function displayWorks(categorieId) {
 
 try {
@@ -41,12 +43,13 @@ try {
             createWorks(works);
         }
     });
+} 
     catch (error) {
         console.log("Erreur durant l'affichage des projets")
     }
-}}
+}
 
-/*** Création des projets dans la gallery ***/
+/***Création des projets dans la gallery***/
 function createWorks(works) {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
@@ -59,4 +62,26 @@ function createWorks(works) {
     figure.appendChild(img);
     figure.appendChild(figcaption);
     gallery.appendChild(figure);
+}
+
+async function buttonfilters() {
+    const dataCategories = await getCategories();
+    const filters = document.querySelector(".filters");
+
+    dataCategories.forEach((category) => {
+        const btnCategorie = document.createElement("button");
+        btnCategorie.innerText = category.name;
+        btnCategorie.setAttribute("class", "btnfilter");
+        btnCategorie.setAttribute("buttonID", category.id);
+        filters.appendChild(btnCategorie);
+
+        /**event listener pour les btn**/
+        btnCategorie.addEventListener('click', function() {
+            /**suppression de la classe 'selected'**/
+            const allButtons = document.querySelectorAll('.btnfilter');
+            allButtons.forEach(button => button.classList.remove('selected'));
+            /**Ajout de la classe 'selected' uniquement sur le btn sélectionné**/
+            btnCategorie.classList.add('selected');
+        });
+    });
 }
