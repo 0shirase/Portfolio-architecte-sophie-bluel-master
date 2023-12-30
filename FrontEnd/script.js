580516a9b1bc1938908b273e9ca9ed9466d216df
0shirase <lucas0999.lb@gmail.com>
2023-12-30 16:47:01 +0100

    ajout de l'ouverture et de la fermeture de la fenêtre modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,12 @@
 const gallery = document.querySelector('.gallery');
 const filters = document.querySelector(".filters");
+const portfoliotitle = document.querySelector(".portfoliotitle");
 
-async function main () {
-    displayWorks ();
-    displayfilters ();
+
+async function main() {
+    displayWorks();
+    displayfilters();
     admin();
-    hideFilterButtons();
-    createModalButton();
 }
 
 main();
@@ -14,9 +14,9 @@ main();
 /***appel de l'API***/
 
 /**works **/
-async function getWorks () {
+async function getWorks() {
     try {
-        const worksResponse = await fetch ("http://localhost:5678/api/works");
+        const worksResponse = await fetch("http://localhost:5678/api/works");
         return worksResponse.json();
     }
     catch (error) {
@@ -25,10 +25,10 @@ async function getWorks () {
 }
 
 /**category**/
-async function getCategories () {
-    
+async function getCategories() {
+
     try {
-        const categoriesResponse = await fetch ("http://localhost:5678/api/categories");
+        const categoriesResponse = await fetch("http://localhost:5678/api/categories");
         return categoriesResponse.json();
     }
     catch (error) {
@@ -38,16 +38,16 @@ async function getCategories () {
 /***Affichage dynamique de la gallery***/
 async function displayWorks(categorieId) {
 
-try {
-    const dataworks = await getWorks();
-    gallery.innerHTML = "";
+    try {
+        const dataworks = await getWorks();
+        gallery.innerHTML = "";
 
-    dataworks.forEach((works) => {
-        if (categorieId == works.category.id || categorieId == null) {
-            createWorks(works);
-        }
-    });
-} 
+        dataworks.forEach((works) => {
+            if (categorieId == works.category.id || categorieId == null) {
+                createWorks(works);
+            }
+        });
+    }
     catch (error) {
         console.log("Erreur durant l'affichage des projets")
     }
@@ -66,7 +66,7 @@ function createWorks(works) {
     figure.appendChild(img);
     figure.appendChild(figcaption);
     gallery.appendChild(figure);
-} 
+}
 
 function createFilter(category) {
     const btnCategorie = document.createElement("button");
@@ -74,115 +74,117 @@ function createFilter(category) {
     btnCategorie.setAttribute("class", "btnfilter");
     btnCategorie.setAttribute("buttonID", category.id);
     filters.appendChild(btnCategorie);
-} 
+}
 
 async function displayfilters() {
     const dataCategories = await getCategories();
-    
+
 
     dataCategories.forEach((category) => {
-        
+
         createFilter(category)
-        
+
     });
 
     const allButtons = document.querySelectorAll('.btnfilter');
 
     allButtons.forEach((btnCategorie) => {
-        
+
         /**event listener pour les btn**/
-        btnCategorie.addEventListener('click', function() {
-            
+        btnCategorie.addEventListener('click', function () {
+
             let categoryId = btnCategorie.getAttribute("buttonID");
 
             /**suppression de la classe 'selected'**/
-        
+
             allButtons.forEach(button => button.classList.remove('selected'));
-        
+
             /**Ajout de la classe 'selected' uniquement sur le btn sélectionné**/
             btnCategorie.classList.add('selected');
-            
+
             /**Affichage des works suivant la catégorie**/
             displayWorks(categoryId);
         });
-        
+
     });
 
-   
+
 }
 
 /**Vérification de la connexion sur index.html**/
-    function admin() {
+function admin() {
     const loginLink = document.getElementById("login");
     const logoutLink = document.getElementById("logout");
 
-    const token = sessionStorage.getItem("token"); 
+    const token = sessionStorage.getItem("token");
     if (token) {
         loginLink.style.display = "none";
         logoutLink.style.display = "block";
 
         /**Gestionnaire d'événement pour la déconnexion**/
-        logoutLink.addEventListener("click", function() {
-            sessionStorage.removeItem("token"); 
-            window.location.href = "./index.html"; 
+        logoutLink.addEventListener("click", function () {
+            sessionStorage.removeItem("token");
+            window.location.href = "./index.html";
         });
 
         /*Création de la bannière*/
-const blackBanner = document.createElement('div');
-blackBanner.style.backgroundColor = 'black';
-blackBanner.style.width = '100%';
-blackBanner.style.height = '40px'; 
-blackBanner.style.display = 'flex';
-blackBanner.style.justifyContent = 'center';
-blackBanner.style.alignItems = 'center';
-blackBanner.style.color = 'white';
+        const blackBanner = document.createElement('div');
+        blackBanner.style.backgroundColor = 'black';
+        blackBanner.style.width = '100%';
+        blackBanner.style.height = '40px';
+        blackBanner.style.display = 'flex';
+        blackBanner.style.justifyContent = 'center';
+        blackBanner.style.alignItems = 'center';
+        blackBanner.style.color = 'white';
 
-/*Création du span pour l'icône et le texte*/
-const iconTextContainer = document.createElement('span');
-iconTextContainer.style.display = 'flex';
-iconTextContainer.style.alignItems = 'center';
-/*Création de l'icône crayon*/
+        /*Création du span pour l'icône et le texte*/
+        const iconTextContainer = document.createElement('span');
+        iconTextContainer.style.display = 'flex';
+        iconTextContainer.style.alignItems = 'center';
+        /*Création de l'icône crayon*/
 
-const icon = document.createElement('i');
-icon.classList.add('fas', 'fa-pen-to-square');
-icon.style.marginRight = '5px'; 
-iconTextContainer.appendChild(icon);
+        const icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-pen-to-square');
+        icon.style.marginRight = '5px';
+        iconTextContainer.appendChild(icon);
 
-/*Création du texte*/
-const textNode = document.createTextNode('Mode édition');
-iconTextContainer.appendChild(textNode);
+        /*Création du texte*/
+        const textNode = document.createTextNode('Mode édition');
+        iconTextContainer.appendChild(textNode);
 
-/*Ajout du span à l'intérieur de la bannière*/
-blackBanner.appendChild(iconTextContainer);
+        /*Ajout du span à l'intérieur de la bannière*/
+        blackBanner.appendChild(iconTextContainer);
 
-/*Ajout de la bannière noire à l'élément banner*/
-banner.appendChild(blackBanner);
+        /*Ajout de la bannière noire à l'élément banner*/
+        banner.appendChild(blackBanner);
 
-/*Fonction pour créer le bouton modal*/
-function createModalButton() {
+        /*zone filtre cachée*/
+        filters.style.display = 'none';
 
-    const modalButton = document.createElement("button");
-    modalButton.innerText = "Ouvrir la fenêtre modale";
-    modalButton.setAttribute("id", "modalButton");
-    modalButton.style.marginBottom = "10px"; 
-    filters.appendChild(modalButton);
+        /*Sélection de la classe portfoliotitle*/
+        const portfolioTitle = document.querySelector('.portfoliotitle');
 
-    /*Event listener de la fenêtre modal*/
-    modalButton.addEventListener('click', openModal);
-}
+        /*Création du bouton "modifié"*/
+        const modal = document.createElement('div');
+        modal.classList.add('modified-button');
+        modal.style.width = '100px'; 
+        modal.style.height = '30px'; 
+        modal.style.display = 'flex';
+        modal.style.justifyContent = 'center';
+        modal.style.alignItems = 'center';
+        modal.style.cursor = 'pointer'; 
+        modal.style.color = 'black';
+        modal.style.fontSize = '15px';
+
+        
+        /*Création du texte dans le bouton*/
+        const buttonText = document.createTextNode('Modifié');
+        modal.appendChild(buttonText);
 
 
-/*Fonction pour masquer les boutons filtres si l'utilisateur est connecté*/
-function hideFilterButtons() {
-    const filterButtons = document.querySelectorAll('.btnfilter');
-    const isAdmin = admin(); 
 
-    if (isAdmin) {
-        filterButtons.forEach(button => {
-            button.style.display = 'none'; 
-        });
-    }
-}
+        /*Ajout du bouton "modifié" à l'élément portfoliotitle*/
+        portfolioTitle.appendChild(modal);
 
     } else {
         loginLink.style.display = "block";
@@ -190,3 +192,33 @@ function hideFilterButtons() {
     }
 };
 
+
+/*Fonction pour ouvrir la fenêtre modale*/
+function openModal() {
+    const modal = document.querySelector('#modal1');
+    modal.style.display = 'block'; 
+    modal.setAttribute('aria-hidden', 'false'); 
+}
+/* événement de clic pour le bouton "Modifié"*/
+const modifiedButton = document.querySelector('.modified-button');
+modifiedButton.addEventListener('click', openModal);
+
+/*création du contenu de la fenêtre modale*/
+const modalWrapper = document.querySelector('.modal-wrapper');
+
+/* contenu de la fenêtre modale*/
+const modalText = document.createElement('p');
+
+/*Ajoutez le texte à la fenêtre modale*/
+modalWrapper.appendChild(modalText);
+
+
+/*Gestionnaire d'événement pour fermer la fenêtre modale*/
+document.querySelector('.close-button').addEventListener('click', closeModal);
+
+/*Fonction pour fermer la fenêtre modale*/
+function closeModal() {
+    const modal = document.querySelector('#modal1');
+    modal.style.display = 'none'; 
+    modal.setAttribute('aria-hidden', 'true'); 
+}
